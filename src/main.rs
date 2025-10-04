d31f9ab1ca35154bcf151f2f0b94392d2a4c42c6
Hayato Kawai <fohte.hk@gmail.com>
2025-10-04 18:55:34 +0900

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(default_value = ".", help = "List of files/directories to format")]
+    paths: Vec<String>,
+
+    #[clap(short, long, help = "Check mode (don't write changes)")]
+    check: bool,
+}
+
+fn find_files(paths: &[String]) -> Vec<String> {
+    paths.to_vec()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if args.check {
+        println!("Check mode engaged. No changes will be made.");
+    }
+
+    let files = find_files(&args.paths);
+
+    println!("Processing the following files: {:?}", files);
 }
